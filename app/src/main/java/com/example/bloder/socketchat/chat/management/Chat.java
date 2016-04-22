2f3bc08b553626f396856371714bbf71a5f5a460
Daniel <wowmegadenis@hotmail.com>
2016-04-22 19:22:18 -0300

    Prepare to implement chat events

--- app/src/main/java/com/example/bloder/socketchat/chat/management/Chat.java
+++ app/src/main/java/com/example/bloder/socketchat/chat/management/Chat.java
@@ -1,7 +1,53 @@
 package com.example.bloder.socketchat.chat.management;
 
+import android.support.annotation.UiThread;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.bloder.socketchat.message.Message;
+
+import java.util.List;
+
 /**
  * Created by bloder on 22/04/16.
  */
-public class Chat {
+public class Chat implements ChatEvents {
+
+    private List<Message> messageList;
+    private RecyclerView messages;
+
+    public Chat(List<Message> messageList, RecyclerView messages) {
+        this.messageList = messageList;
+        this.messages = messages;
+    }
+
+    @UiThread
+    @Override
+    public void addLog(String message) {
+
+    }
+
+    @Override
+    public void addParticipantsLog(int numUsers) {
+
+    }
+
+    @Override
+    public void addMessage(String username, String message) {
+
+    }
+
+    @Override
+    public void addTyping(String username) {
+
+    }
+
+    @Override
+    public void removeTyping(String username) {
+
+    }
+
+    @Override
+    public void attemptSend() {
+
+    }
 }
